32b050cc3f14ed508f938437b75d8bf53632846c
chimingo <115144534+chimingo@users.noreply.github.com>
2022-10-21 11:28:48 +0100

    my ilundary2

--- lib/place_order.dart
+++ lib/place_order.dart
@@ -10,56 +10,104 @@ class _PlaceOrderState extends State<PlaceOrder> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
-
+      appBar: AppBar(
+        title: const Text("Place Order"),
+      ),
       backgroundColor: Colors.white,
-    //  appBar: AppBar(
+     
         body:SafeArea(
-          child: Column(
-            children:[
-            ElevatedButton(
-              onPressed: (){
-              Navigator.pop(context);
-            }, 
-            child:const Text("Return"),
-            ),
-            const Padding(
-              padding: EdgeInsets.all(8.0),
-          
-              child:TextField(
-                decoration:InputDecoration(
-                  border:OutlineInputBorder(),
-                  labelText:"Email",
-                  hintText: "e.g ashiobichioma@gmail.com"),
-
+          child: SingleChildScrollView(
+            child: Column(
+              children:[
+                const Padding(
+                  padding: EdgeInsets.all(2.0),
+                  child: Text("Enter the details of your data laundry and click to proceed",
+                  style: TextStyle(fontSize: 20,fontWeight: FontWeight.w700),
+                  ),
                 ),
-              ),
-              const SizedBox(height: 20),
-                Padding(
-              padding: const EdgeInsets.all(8.0),
+                const Divider(
+                  color: Colors.lightBlue,
+                  thickness: 2,
+                ),
           
-              child:TextFormField(
-                obscureText: true,
-                decoration:const InputDecoration(
-                  border:OutlineInputBorder(),
-                  labelText:"password",
-                  prefixIcon: Icon(Icons.key),
-                  hintText: "Enter strong password"),
-
-            ),
-          ),
-          // const Padding(
-          //     padding: EdgeInsets.all(10.5)),
-          //   child:TextFormField(),
-        
-
-
-
+                // const Padding(
+                // padding: EdgeInsets.all(2.0),
             
-
-  ]
-        
-        ),
+               
+                // ),
+                const Padding(
+                padding: EdgeInsets.all(2.0),
+            
+                child:TextField(
+                  decoration:InputDecoration(
+                    border:OutlineInputBorder(borderSide:BorderSide(width: 20.0) ),
+                    labelText:"cloth type",
+                    labelStyle: TextStyle(color: Colors.blue),
+                    hintText: "e.g T-Shirt"),
+          
+                  ),
+                ),
+                const SizedBox(height: 20),
+                  Padding(
+                padding: const EdgeInsets.all(2.0),
+            
+                child:TextFormField(
+                  decoration:const InputDecoration(
+                    border:OutlineInputBorder(),
+                    labelText:"cloth color",
+                    labelStyle: TextStyle(color: Colors.blue),
+                
+                    hintText: "e.g red and green"),
+          
+              ),
+            ),
+            const SizedBox(height: 20),
+             Padding(
+                padding: const EdgeInsets.all(2.0),
+            
+                child:TextFormField(
+                  keyboardType: TextInputType.number,
+                  
+                  decoration:const InputDecoration(
+                    border:OutlineInputBorder(),
+                    labelText:"Price ₦",
+                    labelStyle: TextStyle(color: Colors.blue),
+                
+                    hintText: " ₦"),
+                    
+          
+              ),
+            ),
+             Padding(
+                padding: const EdgeInsets.all(2.0),
+            
+                child:TextFormField(
+                  keyboardType: TextInputType.number,
+                  //obscureText: true,
+                  decoration:const InputDecoration(
+                    
+                    labelText:"quantity",
+                    labelStyle: TextStyle(fontStyle: FontStyle.italic),
+                  
+                
+                    ),
+                    
+          
+              ),
+            ),
+                  
+               ElevatedButton(
+                onPressed: (){
+                Navigator.pop(context);
+             }, 
+             child:const Text("Proceed"),
+               ),
+            
+          
+          
+                  
+                  ]),
+          ),
          ) );
     
   }
